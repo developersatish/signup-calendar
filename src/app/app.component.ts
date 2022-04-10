6c234ee587502b1a0adceb9c573d833c241db99f
Satish Singh <satishsingh.net@outlook.com>
2022-04-10 15:56:00 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'signup-calendar';
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    password: '',
+    dob: ''
+  }
 }
